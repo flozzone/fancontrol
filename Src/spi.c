d04b8d7ada09521988bdfda169d446774fddeca2
flozzone <flozzone@gmail.com>
2020-04-01 23:12:03 +0200

    Remove Input Data line from Display.
    Signed-off-by: flozzone <flozzone@gmail.com>

--- Src/spi.c
+++ Src/spi.c
@@ -64,17 +64,11 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**SPI1 GPIO Configuration    
     PA5     ------> SPI1_SCK
-    PA6     ------> SPI1_MISO
     PA7     ------> SPI1_MOSI 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_7;
+    GPIO_InitStruct.Pin = OLED_SCK_Pin|OLED_MOSI_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_6;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN SPI1_MspInit 1 */
@@ -96,10 +90,9 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
   
     /**SPI1 GPIO Configuration    
     PA5     ------> SPI1_SCK
-    PA6     ------> SPI1_MISO
     PA7     ------> SPI1_MOSI 
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+    HAL_GPIO_DeInit(GPIOA, OLED_SCK_Pin|OLED_MOSI_Pin);
 
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
 
