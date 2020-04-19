489ae05f95321296ac98b477b36791ccb0b421e4
flozzone <flozzone@gmail.com>
2020-04-19 16:17:11 +0200

    Update STM32CubeMX version and regenerate init
    Signed-off-by: flozzone <flozzone@gmail.com>

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -101,6 +101,11 @@ void HardFault_Handler(void)
     " handler2_address_const: .word prvGetRegistersFromStack    \n"
     );
   /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
 }
 
 /**
